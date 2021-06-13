3b4d2d6b7790d1a15b4c8f23bfc3862655b19100
PickHD <taufikjanuar35@gmail.com>
2021-06-13 13:05:05 +0700

    Init middleware CORS

---
+++ src/middlewares/cors.middleware.go
@@ -0,0 +1,27 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"gin-api-boilerplate/src/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+//*Setup CORS
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			utils.ResponseFormatter(http.StatusOK,"OPTIONS METHOD",nil,nil,c)
+		} else {
+			c.Next()
+		}
+	}
+}
